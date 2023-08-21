9b8f6962e9587b4d5f030c27742a15f14f6371c9
Pablo González <gonpablo@gmail.com>
2023-08-20 22:33:11 -0300

    Cambio

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,6 +26,7 @@ const Footer = () => {
             </FooterNavbar>
           </FooterItem>
 
+
           <FooterItem>
             <FooterTitle>Secciones</FooterTitle>
             <FooterNavbar>
